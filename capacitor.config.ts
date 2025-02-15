7b6491269c417939f00b6edb2bedcc2c93c27326
Paul Bui-Quang <paul.bui-quang@versatile-world.net>
2025-02-15 18:02:37 +0100

    build: add capacitorjs

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'net.upadvipa.supernoteviewer',
+  appName: 'supernote-viewer',
+  webDir: 'dist'
+};
+
+export default config;
